72d832eecf08d7a9b1f37bd33d75ce16bb02a514
Matt <1009003+tantaman@users.noreply.github.com>
2023-07-21 16:31:12 -0400

    fix schemes so they run, add init method

--- xcode-starter/xcode_starterApp.swift
+++ xcode-starter/xcode_starterApp.swift
@@ -9,6 +9,13 @@ import SwiftUI
 
 @main
 struct xcode_starterApp: App {
+    init() {
+        UserDefaults.standard.register(defaults: [
+            "name": "Taylor Swift",
+            "highScore": 10
+        ])
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
